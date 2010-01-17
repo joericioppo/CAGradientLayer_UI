97a5a0ceb1f1c9dda35fd3de66383a3b69b96d27
Joe Ricioppo <spuufmail@mac.com>
2010-01-16 22:45:48 -0700

    Clean

--- Classes/AIBarGradientMatteBottom.m
+++ Classes/AIBarGradientMatteBottom.m
@@ -19,10 +19,10 @@
 		//Colors
 		UIColor *colorOne		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.15 alpha:1.0];
 		UIColor *colorTwo		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.15 alpha:1.0];
-		UIColor *colorThree	= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.8 alpha:1.0];
+		UIColor *colorThree	        = [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.8 alpha:1.0];
 		UIColor *colorFour		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.8 alpha:1.0];
 		UIColor *colorFive		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.5 alpha:1.0];
-		UIColor *colorSix			= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.2 alpha:1.0];
+		UIColor *colorSix		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.2 alpha:1.0];
 		
 		NSArray *colors =  [NSArray arrayWithObjects:(id)colorOne.CGColor, colorTwo.CGColor, colorThree.CGColor, colorFour.CGColor, colorFive.CGColor, colorSix.CGColor, nil];
 		
@@ -30,8 +30,8 @@
 		
 		NSNumber *stopOne		= [NSNumber numberWithFloat:0.00];
 		NSNumber *stopTwo		= [NSNumber numberWithFloat:0.03];
-		NSNumber *stopThree	= [NSNumber numberWithFloat:0.03];
-		NSNumber *stopFour 	= [NSNumber numberWithFloat:0.04];
+		NSNumber *stopThree	        = [NSNumber numberWithFloat:0.03];
+		NSNumber *stopFour 	        = [NSNumber numberWithFloat:0.04];
 		NSNumber *stopFive		= [NSNumber numberWithFloat:0.04];
 		NSNumber *stopSix		= [NSNumber numberWithFloat:1.00];
 		
