a61cea04b81269daa9d0f12b8fceff1f63920808
Joe Ricioppo <spuufmail@mac.com>
2010-01-16 22:37:54 -0700

    Clean

--- Classes/AICellGradient.m
+++ Classes/AICellGradient.m
@@ -17,27 +17,27 @@
 
 //		UIColor *colorOne		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:1.0 alpha:1.0];
 //		UIColor *colorTwo		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.9 alpha:1.0];
-//		UIColor *colorThree	= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.8 alpha:1.0];
+//		UIColor *colorThree	        = [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.8 alpha:1.0];
 //		UIColor *colorFour		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.55 alpha:1.0];
 
 		UIColor *colorOne		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:1.0 alpha:1.0];
 		UIColor *colorTwo		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.85 alpha:1.0];
-		UIColor *colorThree	= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.7 alpha:1.0];
+		UIColor *colorThree	        = [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.7 alpha:1.0];
 		UIColor *colorFour		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.4 alpha:1.0];
 
 //		UIColor *colorOne		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:1.0 alpha:1.0];
 //		UIColor *colorTwo		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.75 alpha:1.0];
-//		UIColor *colorThree	= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.62 alpha:1.0];
+//		UIColor *colorThree	        = [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.62 alpha:1.0];
 //		UIColor *colorFour		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.3 alpha:1.0];	
 	
 //		UIColor *colorOne		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.9 alpha:1.0];
 //		UIColor *colorTwo		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.65 alpha:1.0];
-//		UIColor *colorThree	= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.5 alpha:1.0];
+//		UIColor *colorThree	        = [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.5 alpha:1.0];
 //		UIColor *colorFour		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.2 alpha:1.0];
 
 //		UIColor *colorOne		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.55 alpha:1.0];
 //		UIColor *colorTwo		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.2 alpha:1.0];
-//		UIColor *colorThree	= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.05 alpha:1.0];
+//		UIColor *colorThree	        = [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.05 alpha:1.0];
 //		UIColor *colorFour		= [UIColor colorWithHue:0.625 saturation:0.0 brightness:0.0 alpha:1.0];
 		
 		NSArray *colors =  [NSArray arrayWithObjects:(id)colorOne.CGColor, colorTwo.CGColor, colorThree.CGColor, colorFour.CGColor, nil];
@@ -47,7 +47,7 @@
 
 		NSNumber *stopOne		= [NSNumber numberWithFloat:0.0];
 		NSNumber *stopTwo		= [NSNumber numberWithFloat:0.02];
-		NSNumber *stopThree	= [NSNumber numberWithFloat:0.98];
+		NSNumber *stopThree	        = [NSNumber numberWithFloat:0.98];
 		NSNumber *stopFour		= [NSNumber numberWithFloat:1.0];
 		
 		NSArray *locations = [NSArray arrayWithObjects:stopOne, stopTwo, stopThree, stopFour, nil];
